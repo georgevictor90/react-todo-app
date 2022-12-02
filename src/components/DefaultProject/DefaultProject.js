9b00ee3a62dcc0bf3d41a8394a878bc6bdf78feb
georgevictor90 <georgevictorl@yahoo.com>
2022-12-02 22:43:47 +0200

    add task card elements

--- src/components/DefaultProject/DefaultProject.js
+++ src/components/DefaultProject/DefaultProject.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Bicycle from "./bicycle.svg";
+import {
+  IoRadioButtonOffOutline,
+  IoCheckmarkCircleOutline,
+} from "react-icons/io5";
 
-export default function DefaultProject({ projects }) {
+export default function DefaultProject({ defaultSections, currentProject }) {
+  const taskCards = defaultSections[currentProject].map((task) => {
+    return (
+      <div key={task.title} className="task-card">
+        <div className="task-always-visible">
+          <IoRadioButtonOffOutline className="task-circle" />
+          <div className="task-card-title">{task.title}</div>
+        </div>
+        <div className="task-expandable hidden-element">
+          <p className="task-card-description">{task.description || ""}</p>
+          <span className="task-card-description">{task.date || ""}</span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <section className="section-content">
-      {projects[1].todos.length ? (
-        <div className="tasks-container">here go the task cards</div>
+      {defaultSections[currentProject].length ? (
+        <div className="tasks-container">{taskCards}</div>
       ) : (
         <div className="section-img-and-info">
           <img className="section-image" src={Bicycle} alt="No tasks" />
