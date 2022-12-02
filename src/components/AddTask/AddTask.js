6f6261b79ebff47545502cf1ab12478078a56fc9
georgevictor90 <georgevictorl@yahoo.com>
2022-12-02 23:52:55 +0200

    add controlled inputs to addTask form

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { RxPaperPlane } from "react-icons/rx";
 import { SlClose } from "react-icons/sl";
 import Modal from "react-modal";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 Modal.setAppElement("#root");
 
 export default function AddTask({ projects, modalIsOpen, toggleModal }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [projectFolder, setProjectFolder] = useState("inbox");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+  }
+
+  function handleFolderChange(e) {
+    setProjectFolder(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const task = {
+      title: title,
+      description: description,
+      folder: projectFolder,
+      date: selectedDate,
+      formattedDate: selectedDate.toLocaleDateString("en-GB"),
+    };
+
+    console.log(task);
+  }
+
   return (
     <Modal
       className="modal"
@@ -21,30 +54,43 @@ export default function AddTask({ projects, modalIsOpen, toggleModal }) {
             name="title"
             id="title"
             className="task-title-input"
+            onChange={handleTitleChange}
+            value={title}
             placeholder="e.g. Renew gym subscription"
           />
           <textarea
             name="description"
             id="description"
             className="task-description-input"
+            onChange={handleDescriptionChange}
+            value={description}
             placeholder="Description"
           ></textarea>
         </div>
         <div className="extra-fields">
           <div className="pills">
-            <input
-              type="date"
-              name="date"
-              id="date"
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="dd/MM/yyyy"
+              minDate={new Date()}
               className="due-date-button"
+              showYearDropdown
+              scrollableYearDropdown
             />
-            <select name="select" id="select" className="folder-select-button">
-              <option value="Inbox">Inbox</option>
+            <select
+              value={projectFolder}
+              onChange={handleFolderChange}
+              name="select"
+              id="select"
+              className="folder-select-button"
+            >
+              <option value="inbox">Inbox</option>
             </select>
           </div>
           <div className="item-actions"></div>
         </div>
-        <button className="submit-button">
+        <button onClick={handleSubmit} className="submit-button">
           <RxPaperPlane className="submit-button-icon" />
         </button>
       </form>
