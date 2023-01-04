26c20374e8cdd71f4e743bd2ace30883178d0b30
georgevictor90 <georgevictorl@yahoo.com>
2023-01-04 23:55:26 +0200

    query user collections to show tasks

--- src/components/DefaultProject/DefaultProject.js
+++ src/components/DefaultProject/DefaultProject.js
@@ -5,69 +5,99 @@ import {
   IoCheckmarkCircleOutline,
 } from "react-icons/io5";
 import { db } from "../../firebase-config";
-import { doc, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  deleteDoc,
+  collection,
+  query,
+  where,
+  onSnapshot,
+  getDocs,
+} from "firebase/firestore";
 
-export default function DefaultProject({ currentProject, projects, tasks }) {
+export default function DefaultProject({
+  currentProject,
+  projects,
+  currentUser,
+  userRef,
+  tasksRef,
+  projectsRef,
+}) {
   const [removedCard, setRemovedCard] = useState("");
   const [currentProjectTasks, setCurrentProjectTasks] = useState([]);
+  const [currentProjectId, setCurrentProjectId] = useState(null);
 
   useEffect(() => {
-    setCurrentProjectTasks(
-      tasks.filter((task) => task.folder.includes(currentProject))
-    );
-  }, [tasks, currentProject]);
+    let q;
 
-  let taskCards = [];
+    if (currentProject === "today") {
+      const date = new Date().toLocaleDateString("en-GB");
+      q = query(tasksRef, where("formattedDate", "==", date));
+    } else {
+      q = query(tasksRef, where("projectId", "==", currentProjectId));
+    }
 
-  if (currentProjectTasks.length) {
-    taskCards = currentProjectTasks.map((task) => {
-      return (
-        <div key={task.id} className="task-card">
-          <div className="task-always-visible">
-            {removedCard !== task.id ? (
-              <IoRadioButtonOffOutline
-                data-id={task.id}
-                onClick={toggleRemove}
-                className="task-circle"
-              />
-            ) : (
-              <IoCheckmarkCircleOutline
-                data-id={task.id}
-                onClick={toggleRemove}
-                className="task-circle"
-              />
-            )}
-            <div
-              style={
-                removedCard === task.id
-                  ? { textDecoration: "line-through" }
-                  : null
-              }
-              className="task-card-title"
-            >
-              {task.title}
-            </div>
-          </div>
-          <div className="task-expandable hidden-element">
-            {task.description && (
-              <p className="task-card-description">{task.description}</p>
-            )}
-            <span className="task-card-description">{task.formattedDate}</span>
-          </div>
-        </div>
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      setCurrentProjectTasks(
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
       );
     });
-  }
+
+    return () => unsub();
+  }, [currentProject, currentProjectId, tasksRef]);
+
+  useEffect(() => {
+    const project = projects.find((proj) => proj.name === currentProject);
+    if (project) {
+      setCurrentProjectId(project.id);
+    }
+  }, [currentProject, projects]);
+
+  const taskCards = currentProjectTasks.map((task) => {
+    return (
+      <div key={task.id} className="task-card">
+        <div className="task-always-visible">
+          {removedCard !== task.id ? (
+            <IoRadioButtonOffOutline
+              data-id={task.id}
+              onClick={toggleRemove}
+              className="task-circle"
+            />
+          ) : (
+            <IoCheckmarkCircleOutline
+              data-id={task.id}
+              onClick={toggleRemove}
+              className="task-circle"
+            />
+          )}
+          <div
+            style={
+              removedCard === task.id
+                ? { textDecoration: "line-through" }
+                : null
+            }
+            className="task-card-title"
+          >
+            {task.title}
+          </div>
+        </div>
+        <div className="task-expandable hidden-element">
+          {task.description && (
+            <p className="task-card-description">{task.description}</p>
+          )}
+          <span className="task-card-description">{task.formattedDate}</span>
+        </div>
+      </div>
+    );
+  });
 
   async function deleteTask(id) {
-    const taskDoc = doc(db, "tasks", id);
+    const taskDoc = doc(tasksRef, id);
     await deleteDoc(taskDoc);
   }
 
   function toggleRemove(e) {
-    console.log("toggleRemove");
     const id = e.target.dataset.id;
-    console.log(id);
     setRemovedCard(id);
     setTimeout(() => {
       deleteTask(id);
