2858719077706e4f26f98c96b4ce85f368f8abfe
georgevictor90 <georgevictorl@yahoo.com>
2023-01-04 23:17:28 +0200

    firebase auth

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGOkD1yVsPRHMtpHYy-ucRxxPWm1-ahFU",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore();
+export const auth = getAuth();
