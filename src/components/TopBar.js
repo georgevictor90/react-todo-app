d7a6ef27658a618ffa74d508994f0cde536212cf
georgevictor90 <georgevictorl@yahoo.com>
2023-01-04 23:18:15 +0200

    add signout button

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { auth } from "../firebase-config";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function TopBar({ currentProject }) {
+  const navigate = useNavigate();
+
+  function signOutUser() {
+    signOut(auth)
+      .then(() => {
+        console.log("succesfully signed out");
+        navigate("/");
+      })
+      .catch((error) => console.log(error.message));
+  }
   return (
     <header className="App-header">
       <h3>{currentProject}</h3>
+      <button onClick={signOutUser} className="signout-button">
+        Sign Out
+      </button>
     </header>
   );
 }
